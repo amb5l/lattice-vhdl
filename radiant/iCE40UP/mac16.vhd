3c07897b3539c6f33c526f6da8f3fbe3b113ef9f
Adam Barnes <ambarnes@gmail.com>
2023-03-06 13:51:44 +0000

    Bug fixes for mac16

--- radiant/iCE40UP/mac16.vhd
+++ radiant/iCE40UP/mac16.vhd
@@ -1,8 +1,22 @@
--- mac16.vhd
--- simple VHDL model for simulation purposes only
+--------------------------------------------------------------------------------
+-- mac16.vhd                                                                  --
+--------------------------------------------------------------------------------
+-- (C) Copyright 2023 Adam Barnes <ambarnes@gmail.com>                        --
+-- This file is part of lattice-vhdl. lattice-vhdl is free software: you can  --
+-- redistribute it and/or modify it under the terms of the GNU Lesser General --
+-- Public License as published by the Free Software Foundation, either        --
+-- version 3 of the License, or (at your option) any later version.           --
+-- lattice-vhdl is distributed in the hope that it will be useful, but        --
+-- WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public     --
+-- License for more details. You should have received a copy of the GNU       --
+-- Lesser General Public License along with lattice-vhdl. If not, see         --
+-- https://www.gnu.org/licenses/.                                             --
+--------------------------------------------------------------------------------
 
 library ieee;
-use ieee.std_logic_1164.all;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
 
 entity mac16 is
   generic (
@@ -151,45 +165,50 @@ end entity mac16;
 
 architecture model of mac16 is
 
-  subtype u8 is unsigned(7 downto 0);
-
   signal cfg       : std_logic_vector(24 downto 0);
-
-  signal ci        : std_logic_vector(15 downto 0); -- raw inputs
-  signal ai        : std_logic_vector(15 downto 0);
-  signal bi        : std_logic_vector(15 downto 0);
-  signal di        : std_logic_vector(15 downto 0);
-
-  signal ai_q      : std_logic_vector(15 downto 0); -- registered inputs
-  signal bi_q      : std_logic_vector(15 downto 0);
-  signal ci_q      : std_logic_vector(15 downto 0);
-  signal di_q      : std_logic_vector(15 downto 0);
-
+  signal c_i       : std_logic_vector(15 downto 0); -- raw inputs
+  signal a_i       : std_logic_vector(15 downto 0);
+  signal b_i       : std_logic_vector(15 downto 0);
+  signal d_i       : std_logic_vector(15 downto 0);
+  signal a_q       : std_logic_vector(15 downto 0); -- registered inputs
+  signal b_q       : std_logic_vector(15 downto 0);
+  signal c_q       : std_logic_vector(15 downto 0);
+  signal d_q       : std_logic_vector(15 downto 0);
   signal a         : std_logic_vector(15 downto 0); -- raw or registered inputs
   signal b         : std_logic_vector(15 downto 0);
   signal c         : std_logic_vector(15 downto 0);
   signal d         : std_logic_vector(15 downto 0);
-
   signal ah_x_bh   : std_logic_vector(15 downto 0); -- 8x8 raw outputs
   signal al_x_bh   : std_logic_vector(15 downto 0); -- "
   signal ah_x_bl   : std_logic_vector(15 downto 0); -- "
   signal al_x_bl   : std_logic_vector(15 downto 0); -- "
-
   signal ah_x_bh_q : std_logic_vector(15 downto 0); -- 8x8 pipeline registers
   signal al_x_bh_q : std_logic_vector(15 downto 0); -- "
   signal ah_x_bl_q : std_logic_vector(15 downto 0); -- "
   signal al_x_bl_q : std_logic_vector(15 downto 0); -- "
-
   signal f         : std_logic_vector(15 downto 0); -- 8x8 raw or registered outputs
   signal j         : std_logic_vector(15 downto 0); -- "
   signal k         : std_logic_vector(15 downto 0); -- "
   signal g         : std_logic_vector(15 downto 0); -- "
-
   signal l         : std_logic_vector(31 downto 0); -- 16x16 raw output
   signal l_q       : std_logic_vector(31 downto 0); -- 16x16 registered output
   signal h         : std_logic_vector(31 downto 0); -- 16x16 raw or registered output
-
+  signal w         : std_logic_vector(15 downto 0);
+  signal x         : std_logic_vector(15 downto 0);
+  signal y         : std_logic_vector(15 downto 0);
+  signal z         : std_logic_vector(15 downto 0);
+  signal hci       : std_logic_vector(0 downto 0);
+  signal haccum    : std_logic_vector(16 downto 0);
+  signal lci       : std_logic_vector(0 downto 0);
+  signal laccum    : std_logic_vector(16 downto 0);
+  signal laccumco  : std_logic;
+  signal lco       : std_logic;
+  signal p         : std_logic_vector(15 downto 0);
+  signal q         : std_logic_vector(15 downto 0);
+  signal r         : std_logic_vector(15 downto 0);
+  signal s         : std_logic_vector(15 downto 0);
   signal o         : std_logic_vector(31 downto 0);    -- output vector
+  signal zero      : std_logic := '0';
 
   function str_to_sl(s : string) return std_logic is
   begin
@@ -215,7 +234,7 @@ architecture model of mac16 is
     else
       return "XX";
     end if;
-  end function str_to_sl;
+  end function str_to_slv2;
 
   procedure reg_bus(
     neg_trigger : in  string;
@@ -227,17 +246,44 @@ architecture model of mac16 is
     signal    q : out std_logic_vector
   ) is
   begin
-    if rst = '1' then
-      q <= (others => '0');
-    elsif clk'event and ce = '1' then
-      if (neg_trigger = "0b0" and clk = '1')
-      or (neg_trigger = "0b1" and clk = '0')
-      then
-        q <= d;
+    if clk'event then
+      if clk = '1' then
+        if neg_trigger = "0b0" then
+          if rst = '1' then
+            q <= (others => '0');
+          elsif ce = '0' or hold = '1' then
+            null;
+          elsif ce = '1' and hold = '0' then
+            q <= d;
+          else
+            q <= (others => 'X');
+          end if;
+        elsif neg_trigger = "0b1" then
+          null;
+        else
+          q <= (others => 'X');
+        end if;
+      elsif clk = '0' then
+        if neg_trigger = "0b1" then
+          if rst = '1' then
+            q <= (others => '0');
+          elsif ce = '0' or hold = '1' then
+            null;
+          elsif ce = '1' and hold = '0' then
+            q <= d;
+          else
+            q <= (others => 'X');
+          end if;
+        elsif neg_trigger = "0b0" then
+          null;
+        else
+          q <= (others => 'X');
+        end if;
       else
         q <= (others => 'X');
+      end if;
     end if;
-  end procedure;
+  end procedure reg_bus;
 
 begin
 
@@ -263,115 +309,135 @@ begin
   cfg(24)           <= str_to_sl(   b_signed                 );
 
   -- inputs
-  reg_bus(neg_trigger,irsttop,clk,ce,chold,ci,ci_q);
-  reg_bus(neg_trigger,irsttop,clk,ce,ahold,ai,ai_q);
-  reg_bus(neg_trigger,irstbot,clk,ce,bhold,bi,bi_q);
-  reg_bus(neg_trigger,irstbot,clk,ce,dhold,di,di_q);
-  c <= ci when cfg(0) = '0' else ci_q when cfg(0) = '1' else (others => 'X');
-  a <= ai when cfg(1) = '0' else ai_q when cfg(1) = '1' else (others => 'X');
-  b <= bi when cfg(2) = '0' else bi_q when cfg(2) = '1' else (others => 'X');
-  d <= di when cfg(3) = '0' else di_q when cfg(3) = '1' else (others => 'X');
+  reg_bus(neg_trigger,irsttop,clk,ce,chold,c_i,c_q);
+  reg_bus(neg_trigger,irsttop,clk,ce,ahold,a_i,a_q);
+  reg_bus(neg_trigger,irstbot,clk,ce,bhold,b_i,b_q);
+  reg_bus(neg_trigger,irstbot,clk,ce,dhold,d_i,d_q);
+  c <= c_i when cfg(0) = '0' else c_q when cfg(0) = '1' else (others => 'X');
+  a <= a_i when cfg(1) = '0' else a_q when cfg(1) = '1' else (others => 'X');
+  b <= b_i when cfg(2) = '0' else b_q when cfg(2) = '1' else (others => 'X');
+  d <= d_i when cfg(3) = '0' else d_q when cfg(3) = '1' else (others => 'X');
 
   -- 8x8
-  ah_x_bh <= std_logic_vector(unsigned(a(15 downto 8))*unsigned(b(15 downto 8)));
-  al_x_bh <= std_logic_vector(unsigned(a( 7 downto 0))*unsigned(b(15 downto 8)));
-  ah_x_bl <= std_logic_vector(unsigned(a(15 downto 8))*unsigned(b( 7 downto 0)));
-  al_x_bl <= std_logic_vector(unsigned(a( 7 downto 0))*unsigned(b( 7 downto 0)));
-  reg_bus(neg_trigger,irsttop,clk,ce,zero   ,ax_x_bx,ax_x_bx_q);
-  reg_bus(neg_trigger,irsttop,clk,ce,cfg(22),ax_x_bx,ax_x_bx_q);
-  reg_bus(neg_trigger,irstbot,clk,ce,cfg(22),ax_x_bx,ax_x_bx_q);
-  reg_bus(neg_trigger,irstbot,clk,ce,zero   ,ax_x_bx,ax_x_bx_q);
-  f <= ah_x_bh when cfg(4) = '0' else ax_x_bx_q when cfg(4) = '1' else (others => 'X');
-  j <= al_x_bh when cfg(4) = '0' else ax_x_bx_q when cfg(6) = '1' else (others => 'X');
-  k <= ah_x_bl when cfg(4) = '0' else ax_x_bx_q when cfg(6) = '1' else (others => 'X');
-  g <= al_x_bl when cfg(4) = '0' else ax_x_bx_q when cfg(5) = '1' else (others => 'X');
+  process(a,b,cfg(24 downto 23))
+    function mul8(
+      x : std_logic_vector(7 downto 0);
+      y : std_logic_vector(7 downto 0);
+      xs : std_logic;
+      ys : std_logic
+    ) return std_logic_vector is
+      variable x9 : signed(8 downto 0);
+      variable y9 : signed(8 downto 0);
+      variable m  : signed(17 downto 0);
+    begin
+      x9(7 downto 0) := signed(x);
+      x9(8) := xs and x(7);
+      y9(7 downto 0) := signed(y);
+      y9(8) := ys and y(7);
+      m := x9*y9;
+      return std_logic_vector(m(15 downto 0));
+    end function mul8;
+    variable al : std_logic_vector(7 downto 0);
+    variable ah : std_logic_vector(7 downto 0);
+    variable bl : std_logic_vector(7 downto 0);
+    variable bh : std_logic_vector(7 downto 0);
+  begin
+    al := a(7 downto 0);
+    ah := a(15 downto 8);
+    bl := b(7 downto 0);
+    bh := b(15 downto 8);
+    ah_x_bh <= mul8(ah,bh,cfg(23),cfg(24));
+    al_x_bh <= mul8(al,bh,'0',cfg(24));
+    ah_x_bl <= mul8(ah,bl,cfg(23),'0');
+    al_x_bl <= mul8(al,bl,cfg(22) and cfg(23),cfg(22) and cfg(24));
+  end process;
+  reg_bus(neg_trigger,irsttop,clk,ce,zero   ,ah_x_bh,ah_x_bh_q);
+  reg_bus(neg_trigger,irsttop,clk,ce,cfg(22),al_x_bh,al_x_bh_q);
+  reg_bus(neg_trigger,irstbot,clk,ce,cfg(22),ah_x_bl,ah_x_bl_q);
+  reg_bus(neg_trigger,irstbot,clk,ce,zero   ,al_x_bl,al_x_bl_q);
+  f <= ah_x_bh when cfg(4) = '0' else ah_x_bh_q when cfg(4) = '1' else (others => 'X');
+  j <= al_x_bh when cfg(4) = '0' else al_x_bh_q when cfg(6) = '1' else (others => 'X');
+  k <= ah_x_bl when cfg(4) = '0' else ah_x_bl_q when cfg(6) = '1' else (others => 'X');
+  g <= al_x_bl when cfg(4) = '0' else al_x_bl_q when cfg(5) = '1' else (others => 'X');
 
   -- 16x16
-  process(mux_f,mux_j,mux_k,mux_g,ppc12,ppc23)
-    variable fh : u8;
-    variable fl : u8;
-    variable jh : u8;
-    variable jl : u8;
-    variable kh : u8;
-    variable kl : u8;
-    variable gh : u8;
-    variable gl : u8;
-    variable s1 : unsigned(8 downto 0);
-    variable s2 : unsigned(8 downto 0);
-    variable s3 : unsigned(7 downto 0);
+  process(f,j,k,g)
+    variable jx : unsigned(23 downto 0);
+    variable kx : unsigned(23 downto 0);
+    variable s  : unsigned(31 downto 0);
   begin
-    fh := unsigned(mux_f(15 downto 8));
-    fl := unsigned(mux_f( 7 downto 0));
-    jh := unsigned(mux_j(15 downto 8));
-    jl := unsigned(mux_j( 7 downto 0));
-    kh := unsigned(mux_k(15 downto 8));
-    kl := unsigned(mux_k( 7 downto 0));
-    gh := unsigned(mux_g(15 downto 8));
-    gl := unsigned(mux_g( 7 downto 0));
-    s1 := jl + kl + gh;
-    s2 := fl + jh + kh + s1(8 downto 8);
-    s3 := fh + s2(8 downto 8);
-    l <= std_logic_vector(s3 & s2(7 downto 0) & s1(7 downto 0) & gl);
+    jx(15 downto 0)  := unsigned(j);
+    jx(23 downto 16) := (others => cfg(24) and j(15));
+    kx(15 downto 0)  := unsigned(k);
+    kx(23 downto 16) := (others => cfg(23) and k(15));
+    s :=
+      shift_left(resize(unsigned(f),32),16) +
+      shift_left(resize(jx,32),8) +
+      shift_left(resize(kx,32),8) +
+      resize(unsigned(g),32);
+    l <= std_logic_vector(s);
   end process;
-  reg_bus(neg_trigger,irstbot,clk,ce,c(22),l,l_q);
+  reg_bus(neg_trigger,irstbot,clk,ce,cfg(22),l,l_q);
   h <= l when cfg(7) = '0' else l_q when cfg(7) = '1' else (others => 'X');
 
-  -- accumulators
-  w <= q when when cfg(12) = '0' else c when cfg(12) = '1' else (others => 'X');
+  -- top accumulator
+  w <= q when cfg(12) = '0' else c when cfg(12) = '1' else (others => 'X');
   with cfg(11 downto 10) select x <=
     a                     when "00",
     f                     when "01",
     h(31 downto 16)       when "10",
     (others => z(15))     when "11",
     (others => 'X')       when others;
-  y <= s when when cfg(12) = '0' else d when cfg(12) = '1' else (others => 'X');
-  with cfg(18 downto 17) select z <=
-    b                     when "00",
-    g                     when "01",
-    h(15 downto 0)        when "10",
-    (others => signextin) when "11",
-    (others => 'X')       when others;
-  with cfg(14 downto 13) select hci <=
-    "0"      when "00",
-    "1"      when "01",
+  with cfg(14 downto 13) select hci(0) <=
+    '0'      when "00",
+    '1'      when "01",
     laccumco when "10",
     lco      when "11",
-    "X"      when others;
-  w_addsub_x <=
-    std_logic_vector(unsigned(w)+unsigned(x)+hci) when addsubtop = '0' else
-    std_logic_vector(unsigned(w)-unsigned(x)-hci) when addsubtop = '1' else
+    'X'      when others;
+  haccum <=
+    std_logic_vector(resize(unsigned(w),17)+resize(unsigned(x),17)+resize(unsigned(hci),17)) when addsubtop = '0' else
+    std_logic_vector(resize(unsigned(w),17)-resize(unsigned(x),17)-resize(unsigned(hci),17)) when addsubtop = '1' else
     (others => 'X');
+  accumco <= haccum(16);
   co <=
-        w_addsub_x(16) when addsubtop = '0' else
-    not w_addsub_x(16) when addsubtop = '1' else
+        haccum(16) when addsubtop = '0' else
+    not haccum(16) when addsubtop = '1' else
     'X';
-  accumco <= w_addsub_x(16);
   signextout <= x(15);
-  with cfg(21 downto 20) select lci <=
-    "0"     when "00",
-    "1"     when "01",
-    accumci when "10",
-    ci      when "11",
-    "X"     when others;
-  y_addsub_z <=
-    std_logic_vector(unsigned(y)+unsigned(z)+lci) when addsubbot = '0' else
-    std_logic_vector(unsigned(y)-unsigned(z)-lci) when addsubbot = '1' else
-    (others => 'X');
-  lco <=
-        y_addsub_z(16) when addsubbot = '0' else
-    not y_addsub_z(16) when addsubbot = '1' else
-    'X';
-  laccumco <= y_addsub_z(16);
-  p <= w_addsub_x when oloadtop = '0' else c when oloadtop = '1' else (others => 'X');
-  r <= y_addsub_z when oloadbot = '0' else d when oloadbot = '1' else (others => 'X');
+  p <= haccum(15 downto 0) when oloadtop = '0' else c when oloadtop = '1' else (others => 'X');
   reg_bus(neg_trigger,orsttop,clk,ce,oholdtop,p,q);
-  reg_bus(neg_trigger,orstbot,clk,ce,oholdbot,r,s);
   with cfg(9 downto 8) select o(31 downto 16) <=
     p               when "00",
     q               when "01",
     f               when "10",
     h(31 downto 16) when "11",
     (others => 'X') when others;
+
+  -- bottom accumulator
+  y <= s when cfg(12) = '0' else d when cfg(12) = '1' else (others => 'X');
+  with cfg(18 downto 17) select z <=
+    b                     when "00",
+    g                     when "01",
+    h(15 downto 0)        when "10",
+    (others => signextin) when "11",
+    (others => 'X')       when others;
+  with cfg(21 downto 20) select lci(0) <=
+    '0'     when "00",
+    '1'     when "01",
+    accumci when "10",
+    ci      when "11",
+    'X'     when others;
+  laccum <=
+    std_logic_vector(resize(unsigned(y),17)+resize(unsigned(z),17)+resize(unsigned(lci),17)) when addsubtop = '0' else
+    std_logic_vector(resize(unsigned(y),17)-resize(unsigned(z),17)-resize(unsigned(lci),17)) when addsubtop = '1' else
+    (others => 'X');
+  laccumco <= laccum(16);
+  lco <=
+        laccum(16) when addsubbot = '0' else
+    not laccum(16) when addsubbot = '1' else
+    'X';
+  r <= laccum(15 downto 0) when oloadbot = '0' else d when oloadbot = '1' else (others => 'X');
+  reg_bus(neg_trigger,orstbot,clk,ce,oholdbot,r,s);
   with cfg(16 downto 15) select o(15 downto 0) <=
     r               when "00",
     s               when "01",
@@ -380,70 +446,70 @@ begin
     (others => 'X') when others;
 
   -- vector stuffing and unstuffing
-  ai(0)   <= a0;
-  ai(1)   <= a1;
-  ai(2)   <= a2;
-  ai(3)   <= a3;
-  ai(4)   <= a4;
-  ai(5)   <= a5;
-  ai(6)   <= a6;
-  ai(7)   <= a7;
-  ai(8)   <= a8;
-  ai(9)   <= a9;
-  ai(10) <= a10;
-  ai(11) <= a11;
-  ai(12) <= a12;
-  ai(13) <= a13;
-  ai(14) <= a14;
-  ai(15) <= a15;
-  bi(0)   <= b0;
-  bi(1)   <= b1;
-  bi(2)   <= b2;
-  bi(3)   <= b3;
-  bi(4)   <= b4;
-  bi(5)   <= b5;
-  bi(6)   <= b6;
-  bi(7)   <= b7;
-  bi(8)   <= b8;
-  bi(9)   <= b9;
-  bi(10) <= b10;
-  bi(11) <= b11;
-  bi(12) <= b12;
-  bi(13) <= b13;
-  bi(14) <= b14;
-  bi(15) <= b15;
-  ci(0)   <= c0;
-  ci(1)   <= c1;
-  ci(2)   <= c2;
-  ci(3)   <= c3;
-  ci(4)   <= c4;
-  ci(5)   <= c5;
-  ci(6)   <= c6;
-  ci(7)   <= c7;
-  ci(8)   <= c8;
-  ci(9)   <= c9;
-  ci(10) <= c10;
-  ci(11) <= c11;
-  ci(12) <= c12;
-  ci(13) <= c13;
-  ci(14) <= c14;
-  ci(15) <= c15;
-  di(0)   <= d0;
-  di(1)   <= d1;
-  di(2)   <= d2;
-  di(3)   <= d3;
-  di(4)   <= d4;
-  di(5)   <= d5;
-  di(6)   <= d6;
-  di(7)   <= d7;
-  di(8)   <= d8;
-  di(9)   <= d9;
-  di(10) <= d10;
-  di(11) <= d11;
-  di(12) <= d12;
-  di(13) <= d13;
-  di(14) <= d14;
-  di(15) <= d15;
+  a_i(0)   <= a0;
+  a_i(1)   <= a1;
+  a_i(2)   <= a2;
+  a_i(3)   <= a3;
+  a_i(4)   <= a4;
+  a_i(5)   <= a5;
+  a_i(6)   <= a6;
+  a_i(7)   <= a7;
+  a_i(8)   <= a8;
+  a_i(9)   <= a9;
+  a_i(10) <= a10;
+  a_i(11) <= a11;
+  a_i(12) <= a12;
+  a_i(13) <= a13;
+  a_i(14) <= a14;
+  a_i(15) <= a15;
+  b_i(0)   <= b0;
+  b_i(1)   <= b1;
+  b_i(2)   <= b2;
+  b_i(3)   <= b3;
+  b_i(4)   <= b4;
+  b_i(5)   <= b5;
+  b_i(6)   <= b6;
+  b_i(7)   <= b7;
+  b_i(8)   <= b8;
+  b_i(9)   <= b9;
+  b_i(10) <= b10;
+  b_i(11) <= b11;
+  b_i(12) <= b12;
+  b_i(13) <= b13;
+  b_i(14) <= b14;
+  b_i(15) <= b15;
+  c_i(0)   <= c0;
+  c_i(1)   <= c1;
+  c_i(2)   <= c2;
+  c_i(3)   <= c3;
+  c_i(4)   <= c4;
+  c_i(5)   <= c5;
+  c_i(6)   <= c6;
+  c_i(7)   <= c7;
+  c_i(8)   <= c8;
+  c_i(9)   <= c9;
+  c_i(10) <= c10;
+  c_i(11) <= c11;
+  c_i(12) <= c12;
+  c_i(13) <= c13;
+  c_i(14) <= c14;
+  c_i(15) <= c15;
+  d_i(0)   <= d0;
+  d_i(1)   <= d1;
+  d_i(2)   <= d2;
+  d_i(3)   <= d3;
+  d_i(4)   <= d4;
+  d_i(5)   <= d5;
+  d_i(6)   <= d6;
+  d_i(7)   <= d7;
+  d_i(8)   <= d8;
+  d_i(9)   <= d9;
+  d_i(10) <= d10;
+  d_i(11) <= d11;
+  d_i(12) <= d12;
+  d_i(13) <= d13;
+  d_i(14) <= d14;
+  d_i(15) <= d15;
   o0     <= o(0);
   o1     <= o(1);
   o2     <= o(2);
