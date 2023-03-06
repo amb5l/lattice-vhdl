5e8b76cd6a05407f50e7f7e530efab61f5e14e50
Adam Barnes <ambarnes@gmail.com>
2023-03-06 13:51:32 +0000

    Move package

--- radiant/iCE40UP/iCE40UP.vhd
+++ radiant/iCE40UP/iCE40UP.vhd
@@ -1,3 +1,19 @@
+--------------------------------------------------------------------------------
+-- iCE40UP.vhd                                                                --
+--------------------------------------------------------------------------------
+-- (C) Copyright 2023 Adam Barnes <ambarnes@gmail.com>                        --
+-- This file is part of lattice-vhdl. lattice-vhdl is free software: you can  --
+-- redistribute it and/or modify it under the terms of the GNU Lesser General --
+-- Public License as published by the Free Software Foundation, either        --
+-- version 3 of the License, or (at your option) any later version.           --
+-- lattice-vhdl is distributed in the hope that it will be useful, but        --
+-- WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public     --
+-- License for more details. You should have received a copy of the GNU       --
+-- Lesser General Public License along with lattice-vhdl. If not, see         --
+-- https://www.gnu.org/licenses/.                                             --
+--------------------------------------------------------------------------------
+
 library ieee;
 use ieee.std_logic_1164.all;
 
